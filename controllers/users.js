54a481f4a4adfe1d729bbcd08332f35b33448e9e
lokendradevloper797 <lokendrasinghrajput982@gmail.com>
2024-09-29 20:52:55 +0530

    changes in users and lead file

--- controllers/users.js
+++ controllers/users.js
@@ -5,12 +5,8 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'sales', 'manager'], default: 'sales' },
 }, { timestamps: true });
 
-// Password hashing middleware
-// lokendra rajputcl 
 
 // Password verification method
 UserSchema.methods.matchPassword = async function(enteredPassword) {
