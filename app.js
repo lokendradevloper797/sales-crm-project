1d69447923b057cc58df4b7fe2f1b9633a5cc353
lokendradevloper797 <lokendrasinghrajput982@gmail.com>
2024-09-29 19:53:03 +0530

    Modified app.js and deleted leadController.js

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-// routes/auth.js
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const asyncHandler = require('express-async-handler');
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 4000;
 
-// Register
+mongoose.connect("mongodb://localhost:27017/salesCRM", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
+
+// Import Routes
+// const customerRoutes = require('./routes/customerRoutes');
+// app.use('/api/customers', customerRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 router.post('/register', asyncHandler(async (req, res) => {
   const { name, email, password, role } = req.body;
   const userExists = await User.findOne({ email });
@@ -31,7 +47,6 @@ router.post('/register', asyncHandler(async (req, res) => {
   }
 }));
 
-// Login
 router.post('/login', asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -57,4 +72,6 @@ const generateToken = (id) => {
   });
 };
 
-module.exports = router;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
